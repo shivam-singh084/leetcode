889b2c7561f72ea27adba20e03eafb90c4856a9e
Shivam Kumar Singh <kumarshivam27399@gmail.com>
2025-05-26 17:26:31 +0530

    Time: 179 ms (39.86%), Space: 147.4 MB (62.74%) - LeetHub

---
+++ 0220-contains-duplicate-iii/0220-contains-duplicate-iii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool containsNearbyAlmostDuplicate(vector<int>& nums, int indexDiff, int valueDiff) {
+        int n = nums.size();
+        set<long long> s;
+        for (int i = 0; i < n; i++) {
+            if (i > indexDiff) {
+                s.erase(nums[i - indexDiff - 1]);
+            }
+            auto a = s.lower_bound((nums[i]) - valueDiff);
+            if (a != s.end() && ((*a) - nums[i]) <= valueDiff) {
+                return true;
+            }
+            s.insert(nums[i]);
+        }
+        return false;
+    }
+};
