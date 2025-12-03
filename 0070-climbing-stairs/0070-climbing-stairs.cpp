4b02b106ae4a7d24436864c9c2799a3a2c68e3b4
Shivam Kumar Singh <aids23044@glbitm.ac.in>
2025-12-03 13:46:10 +0530

    Time: 0 ms (100%), Space: 8.4 MB (47.91%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n == 0 || n == 1) {
-            return 1;
+        vector<int> dp(n+1, 0);
+        dp[0] = dp[1] = 1;
+
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
         }
-        int prev = 1, curr = 1;
-        for (int i = 2; i <= n; i++) {
-            int temp = curr;
-            curr = prev + curr;
-            prev = temp;
-        }
-        return curr;
+        return dp[n];
     }
 };
