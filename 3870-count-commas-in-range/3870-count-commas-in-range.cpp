bd4d343b110626c9c133c378a227de64bf403c14
Shivam Kumar Singh <aids23044@glbitm.ac.in>
2026-09-08 19:50:52 +0530

    Time: 0 ms (100%), Space: 8.5 MB (51.38%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
     int countCommas(int n) {
-        string s = to_string(n);
-        if(s.length() < 4){
-            return 0;
-        }
+        if(n < 1000) return 0;
         return (n-1000)+1;
     }
 };
